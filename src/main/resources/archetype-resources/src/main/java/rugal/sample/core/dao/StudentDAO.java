1720737ed8eef18b32fa1103c9d763ac0c3384c6
Rugal <rugal.bernstein.0@gmail.com>
2016-07-28 20:17:03 -0400

    damn I missed some files

---
+++ src/main/resources/archetype-resources/src/main/java/rugal/sample/core/dao/StudentDAO.java
@@ -0,0 +1,23 @@
+package rugal.sample.core.dao;
+
+import ml.rugal.sshcommon.hibernate.Updater;
+import ml.rugal.sshcommon.page.Pagination;
+import rugal.sample.core.entity.Student;
+
+/**
+ *
+ * @author Rugal Bernstein
+ */
+public interface StudentDAO
+{
+
+    Student delete(Student bean);
+
+    Student get(Integer id);
+
+    Pagination getPage(int pageNo, int pageSize);
+
+    Student save(Student bean);
+
+    Student updateByUpdater(Updater<Student> updater);
+}
